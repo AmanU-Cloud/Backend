7e2665cd1311f07821dfbeafe8c448828a53c007
SergeyAnosov <saanosov@istokmw.ru>
2025-10-27 13:08:21 +0300

    "NS-docker-compose" Заполнил Dockerfile, docker-compose.yaml, .env, добавил hello эндпоинт

--- cmd/reviewer/main.go
+++ cmd/reviewer/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %V", err)
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello Caritas"))
+	})
+
+	port := os.Getenv("SERVER_PORT")
+
+	fmt.Println("Listening on :8081")
+	err = http.ListenAndServe(":"+port, mux)
+	if err != nil {
+		log.Fatalf("Error starting HTTP server: %V", err)
+	}
+	fmt.Println("Server has started")
 }
