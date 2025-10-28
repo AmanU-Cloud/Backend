35d75693a24cfc8ba43aa95a7d497b924d8556da
sacred-thinker <yorik11@gmail.com>
2025-10-28 14:36:31 +0300

    Fix linter error

--- cmd/reviewer/main.go
+++ cmd/reviewer/main.go
@@ -1,52 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	"gopkg.in/yaml.v3"
+	"github.com/google/uuid" //добавил и использовал, чтобы появился go.sum
 )
 
-type conf struct {
-	Server struct {
-		Port string `yaml:"port"`
-		Host string `yaml:"host"`
-	}
-}
-
-func (c *conf) getConf() *conf {
-	yamlFile, err := os.ReadFile("cfg/config.yaml")
-	if err != nil {
-		log.Printf("yamlFile.Get err   #%v ", err)
-	}
-	err = yaml.Unmarshal(yamlFile, c)
-	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
-	}
-	return c
-}
-
 func main() {
-
-	var config conf
-	config.getConf()
-
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("hello Caritas"))
-		if err != nil {
-			return
-		}
-	})
-	port := config.Server.Port
-	host := config.Server.Host
-
-	fmt.Printf("Listening on %v : %v", host, port)
-	err := http.ListenAndServe(host+":"+port, mux)
-	if err != nil {
-		log.Fatalf("Error starting HTTP server: %V", err)
-	}
+	_ = uuid.New()
 }
