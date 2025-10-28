09e2d128ea52862c06b1ebe27bb60bdfe96b645e
SergeyAnosov <Anosov-sa@yandex.ru>
2025-10-28 21:49:20 +0300

    NS-8 добавил memecached в docker-compose, добавил эндпоинт-пример с методом пузырька, в url надо задать размер массива

--- cmd/reviewer/main.go
+++ cmd/reviewer/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"Caritas-Backend/internal/memecached"
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
+	"github.com/bradfitz/gomemcache/memcache"
 	"gopkg.in/yaml.v3"
 )
 
@@ -13,6 +19,10 @@ type conf struct {
 	Server struct {
 		Port string `yaml:"port"`
 		Host string `yaml:"host"`
+	}
+	Memcached struct {
+		Servers string `yaml:"servers"`
+		Ttl     int64  `yaml:"ttl"`
 	}
 }
 import (
@@ -47,6 +57,17 @@ func main() {
 			return
 		}
 	})
+
+	//эндпоинт для memcache GET localhost:8080/bubble?n=100000.
+	//вызывать повторно, ttl - 1 минута
+	//работает только через докер
+	mux.HandleFunc("/bubble", bubbleHandler)
+
+	cachePort := config.Memcached.Servers
+	cacheTTL := config.Memcached.Ttl
+	fmt.Println("config", config)
+	memecached.Init(cachePort, time.Duration(cacheTTL)*time.Second)
+
 	port := config.Server.Port
 	host := config.Server.Host
 
@@ -56,4 +77,69 @@ func main() {
 		log.Fatalf("Error starting HTTP server: %V", err)
 	}
 	fmt.Println("Server has started")
+}
+
+func bubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
+
+func bubbleHandler(w http.ResponseWriter, r *http.Request) {
+	str := r.URL.Query().Get("n")
+	if str == "" {
+		http.Error(w, "missing query param 'n'", http.StatusBadRequest)
+		return
+	}
+
+	n, err := strconv.Atoi(str)
+	if err != nil {
+		http.Error(w, "invalid 'n': must be integer", http.StatusBadRequest)
+		return
+	}
+
+	cacheKey := "bubble:" + str
+
+	// Пытаемся получить из кеша
+	if item, err := memecached.Client().Get(cacheKey); err == nil {
+		fmt.Println("cache has data")
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("X-Cache", "HIT")
+		w.Write(item.Value)
+		return
+	}
+
+	// Генерируем массив
+	arr := make([]int, n)
+	for i, value := range rand.Perm(n) {
+		arr[i] = value
+	}
+	fmt.Println(arr)
+
+	bubbleSort(arr)
+
+	jsonData, err := json.Marshal(arr)
+	if err != nil {
+		http.Error(w, "failed to marshal result", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("cache has No data")
+	err = memecached.Client().Set(&memcache.Item{
+		Key:        cacheKey,
+		Value:      jsonData,
+		Expiration: int32(memecached.DefaultTTL().Seconds()),
+	})
+	if err != nil {
+		http.Error(w, "failed to set cache", http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Cache", "MISS")
+	w.Write(jsonData)
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Backend/reviewer
 go 1.25
 
 require github.com/google/uuid v1.6.0
+
+require github.com/bradfitz/gomemcache v0.0.0-20250403215159-8d39553ac7cf // indirect
