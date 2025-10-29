8b988424d64b383cb210ff9a3e4a41d244d045b1
Александр Музуров <muzurov26@gmail.com>
2025-10-29 07:21:23 +0300

    [NS-6] Добавил prometheus в отдельной директории с множеством метрик и функции для их обновления

--- cmd/reviewer/main.go
+++ cmd/reviewer/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/Backend/internal/metrics"
+)
+
+func main() {
+	metrics.InitMetrics()
+
+	log.Println("Server run on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatalf("Server run error: %v", err)
+	}
 }
