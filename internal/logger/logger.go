69cc330f1198aed8bfacad0de5795a71eab26439
Александр Музуров <muzurov26@gmail.com>
2025-10-30 01:17:14 +0300

    [NS-7] Глобальная переменная и методы

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -5,11 +5,35 @@ import (
 	"os"
 )
 
+var GlobalLogger *slog.Logger
+
 func init() {
 	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		AddSource: true,           // Место возникновения ошибки
-		Level:     slog.LevelInfo, // Уровень логирования
+		AddSource: true,
+		Level:     slog.LevelInfo,
 	})
-	globalLogger := slog.New(handler)
-	slog.SetDefault(globalLogger)
+	GlobalLogger = slog.New(handler)
+	slog.SetDefault(GlobalLogger)
+}
+
+// Методы логирования
+
+// Info записывает инфо-сообщение
+func Info(msg string, args ...any) {
+	GlobalLogger.Info(msg, args...)
+}
+
+// Warn записывает предупреждение
+func Warn(msg string, args ...any) {
+	GlobalLogger.Warn(msg, args...)
+}
+
+// Debug записывает дебаг-сообщение
+func Debug(msg string, args ...any) {
+	GlobalLogger.Debug(msg, args...)
+}
+
+// Error записывает ошибку
+func Error(msg string, args ...any) {
+	GlobalLogger.Error(msg, args...)
 }
