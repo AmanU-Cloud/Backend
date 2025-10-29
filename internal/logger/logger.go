6a8f6eecb8c08957b400efa0d605adf4f4b296a8
Александр Музуров <muzurov26@gmail.com>
2025-10-29 16:42:18 +0300

    [NS-7]Добавлен глобальный логгер

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,15 @@
 package logger
+
+import (
+	"log/slog"
+	"os"
+)
+
+func init() {
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,           // Место возникновения ошибки
+		Level:     slog.LevelInfo, // Уровень логирования
+	})
+	globalLogger := slog.New(handler)
+	slog.SetDefault(globalLogger)
+}
