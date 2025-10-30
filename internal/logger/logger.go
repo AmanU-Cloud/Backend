89aafb47fbadd210fdd253a0bc470b8d9c75446c
Александр Музуров <muzurov26@gmail.com>
2025-10-30 05:26:11 +0300

    [NS-7] Получение уровня и формата из config

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,17 +1,100 @@
 package logger
 
 import (
+	"bytes"
+	"fmt"
 	"log/slog"
 	"os"
+
+	yaml "gopkg.in/yaml.v3"
 )
 
 var GlobalLogger *slog.Logger
 
+// Структура для представления конфигурации
+type Config struct {
+	Logging struct {
+		Level  string `yaml:"level"`
+		Format string `yaml:"format"`
+	} `yaml:"logging"`
+}
+
+// Чтение и обработка конфигурации
+func loadConfig(path string) (*Config, error) {
+	fd, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка при открытии файла конфигурации: %w", err)
+	}
+	defer fd.Close()
+
+	buf := bytes.Buffer{}
+	_, err = buf.ReadFrom(fd)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка при чтении файла конфигурации: %w", err)
+	}
+
+	var cfg Config
+	err = yaml.Unmarshal(buf.Bytes(), &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка при разборе конфигурации: %w", err)
+	}
+
+	return &cfg, nil
+}
+
 func init() {
-	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
-		AddSource: true,
-		Level:     slog.LevelInfo,
-	})
+	cfg, err := loadConfig("../cfg/config.yml") // Относительный путь к файлу конфигурации
+	if err != nil {
+		slog.Error("Ошибка при загрузке (использованы настройки по умолчанию):", slog.Any("error", err))
+		// Используем стандартные настройки
+		cfg = &Config{
+			Logging: struct {
+				Level  string `yaml:"level"`
+				Format string `yaml:"format"`
+			}{
+				Level:  "info",
+				Format: "json", // Значение по умолчанию
+			},
+		}
+	}
+
+	// Определяем уровень логирования
+	var level slog.Level
+	switch cfg.Logging.Level {
+	case "debug":
+		level = slog.LevelDebug
+	case "info":
+		level = slog.LevelInfo
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		slog.Warn("Указанный уровень логирования не распознается, используется Debug по умолчанию.")
+		level = slog.LevelDebug // Устанавливаем уровень по умолчанию
+	}
+
+	// Определяем формат логирования
+	var handler slog.Handler
+	switch cfg.Logging.Format {
+	case "json":
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource: true,
+			Level:     level,
+		})
+	case "text": // Возможно, поддержка текстового формата
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource: true,
+			Level:     level,
+		})
+	default:
+		slog.Warn("Формат логирования не найден, использован JSON по умолчанию")
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource: true,
+			Level:     level,
+		})
+	}
+
 	GlobalLogger = slog.New(handler)
 	slog.SetDefault(GlobalLogger)
 }
