d7765e2b9a782f65258c303dc892c345b0d7db86
Александр Музуров <muzurov26@gmail.com>
2025-11-04 23:35:41 +0300

    NS-7: убран лишний файл в cfg

--- cfg/cfgStruct.go
+++
@@ -1,9 +0,0 @@
-package cfg
-
-// Структура для представления конфигурации логгера
-type Config struct {
-	Logging struct {
-		Level  string `yaml:"level"`  // Уровень логирования
-		Format string `yaml:"format"` // Формат логирования
-	} `yaml:"logging"`
-}
